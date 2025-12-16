5c5f2e9e0756dc700c35538e02211e0e3ee33a62
Mahdi Hadj Taieb <mahdi.ht2018@gmail.com>
2025-12-16 16:55:59 +0100

    UP my solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -186,7 +186,7 @@ class MonthlySplit(BaseCrossValidator):
 
         months = sorted(list(set([(d.year, d.month) for d in dates])))
         return max(0, len(months) - 1)
-    
+
     def split(self, X, y, groups=None):
         """Generate indices to split data into training and test set.
 
@@ -231,3 +231,4 @@ class MonthlySplit(BaseCrossValidator):
                 elif (date.year, date.month) == test_month:
                     idx_test.append(idx)
             yield (np.array(idx_train), np.array(idx_test))
+            
